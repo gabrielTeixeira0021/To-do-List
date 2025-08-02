7cd46915c9f60e2453a604b39ae97ffbe1055eaa
hyperGT <charlesgabriel102@gmail.com>
2025-08-02 18:45:25 -0300

    feat: Controller Initial code with Get and Post Methods
    Co-authored-by: Davy <davy.dama55@gmail.com>

---
+++ Server-Side/src/main/java/com/list/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.list.controller;
+
+import com.list.domain.User;
+import com.list.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User newUser) {
+        User createdUser = userService.createUser(newUser);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
